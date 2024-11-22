1e2ad7fcf72f97f1152f07330d9453050914a723
fardiansyah <fardiansyah@ifca.co.id>
2024-11-23 00:10:55 +0700

    finish: end

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 
 export default function Page() {
   return <div className="text-2xl font-bold text-gray-800">Customers Page</div>;
